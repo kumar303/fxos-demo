b263dbd16f5c08662fcc7e9c2815b884c8f9df0b
Kumar McMillan <kumar.mcmillan@gmail.com>
2012-11-11 17:08:29 +0000

    Fiddled with JS to make pkg app work; gave up

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 $(document).ready(function(){
-   $.getJSON("http://search.twitter.com/search.json?rpp=100&callback=?&q=%23mozfest",
-       function(data){
+    $.ajax({
+      url: "http://search.twitter.com/search.json",
+      dataType: 'jsonp',
+      data: {rpp: 100,
+             q: '#mozfest'},
+      error: function() {
+          console.error('jsonp failed');
+      },
+      // This might make things work as a package app. Also needs
+      //
+      //  "permissions": {
+      //      "systemXHR": {}
+      //   },
+      //xhrFields: {mozSystem: true},
+      success: function(data){
         for(var i=0;i<data.results.length;i++){
             $('#result').prepend('<div class="tweet">' +
                '<img src="'+data.results[i].profile_image_url+'" width="50" height="60"/>' +
                '<span id="tweetText">'+data.results[i].text+'</span>' +
                '</div>');
         }
- });
+      }
+    });
 });
 
 console.log('loaded app');
