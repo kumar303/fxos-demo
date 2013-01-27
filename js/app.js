6af39e74ee67601af7e0ab1d6bc176f97b3e1da7
Kumar McMillan <kumar.mcmillan@gmail.com>
2013-01-26 18:13:37 -0600

    Switched hashtag to #FirefoxOS

--- js/app.js
+++ js/app.js
@@ -1,27 +1,38 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var links = /(http:\/\/[^\s]+)/gi;
     $.ajax({
       url: "http://search.twitter.com/search.json",
       dataType: 'jsonp',
       data: {rpp: 100,
-             q: '#mozfest'},
+             q: '#FirefoxOS'},
       error: function() {
           console.error('jsonp failed');
       },
-      // This might make things work as a package app.
-      // The manifest also needs
+      //
+      // To make this work as a package app, you need to add
+      // this parameter to the xhr:
+      //
+      //xhrFields: {mozSystem: true},
+      //
+      // You also need to add this permission to your manifest:
+      //
       //  "permissions": {
       //      "systemXHR": {}
       //   },
-      //xhrFields: {mozSystem: true},
-      success: function(data){
+      //
+      success: function(data) {
+        var body;
         $('#load-progress').hide();
         for (var i=0; i < data.results.length; i++) {
+            body = data.results[i].text;
+            body += ' ';  // terrible hack to make the HTML regex easier.
+            body = body.replace(links, '<a target="_blank" href="$1">$1</a>');
             $('#result').prepend(
                '<li data-state="tagged" data-tag="A">' +
                  '<div class="tweet">' +
                    '<img src="'+data.results[i].profile_image_url+'">' +
                    '<dl><dt>' +
-                     '<span id="tweetText">'+data.results[i].text+'</span>' +
+                     '<span id="tweetText">'+body+'</span>' +
                    '</dt></dl>' +
                  '</div>' +
                '</li>');
